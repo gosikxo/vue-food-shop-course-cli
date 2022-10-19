e0f8d27ee295d2b0a5bdc8fa2783c9d58f744c3b
Gosia Brozek <gosikbrozek1997@gmail.com>
2022-10-19 15:18:00 +0300

    Add sidebar from previous project

--- src/App.vue
+++ src/App.vue
@@ -17,9 +17,44 @@
       <span>Cart ({{ totalQuantity }})</span>
     </router-link> -->
   </header>
-  <router-view />
+  <router-view :inventory="inventory" />
+  <Sidebar v-if="showSidebar" :toggle="toggleSidebar" :cart="cart" :inventory="inventory" :remove="removeItem" />
 </template>
 
-<style>
+<script>
+import Sidebar from '@/components/Sidebar.vue'
+import food from '@/food.json'
 
-</style>
+export default {
+  components: {
+    Sidebar
+  },
+  data () {
+    return {
+      showSidebar: true,
+      inventory: food,
+      cart: {}
+    }
+  },
+  computed: {
+    totalQuantity () {
+      return Object.values(this.cart).reduce((acc, curr) => {
+        return acc + curr
+      }, 0)
+    }
+  },
+  methods: {
+    addToCart (name, index) {
+      if (!this.cart[name]) this.cart[name] = 0
+      this.cart[name] += this.inventory[index].quantity
+      this.inventory[index].quantity = 0
+    },
+    toggleSidebar () {
+      this.showSidebar = !this.showSidebar
+    },
+    removeItem (name) {
+      delete this.cart[name]
+    }
+  }
+}
+</script>
